1240f45a0cec1aab4ea1a9e0bbd0e1303fc8e7d3
Maung Ko Ko <mwkoko2020@gmail.com>
2023-04-26 16:38:09 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById('main');
+    const newHeader = document.createElement('h1');
+    main.remove();
+    newHeader.id= 'victory';
+    newHeader.textContent = 'Baker is the champion';
+    document.body.append(newHeader);
